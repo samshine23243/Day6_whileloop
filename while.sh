3d86fc4f8f03738378c8c5def625cd200dcf9336
samshine <sameershinde23243@gmail.com>
2021-02-15 15:28:52 +0530

    example2 code added

--- while.sh
+++ while.sh
@@ -1,1 +1,27 @@
 echo "while loop problems"
+read -p "Think Number(1-10) : " N
+start=1
+end=10
+temp2=0
+yes=1
+no=0
+while [ $temp2 -ne $N ]
+do
+   temp1=$(($start+$end))
+   temp2=$(($temp1/2))
+   echo "is number $temp2(yes 1/no 0):"
+   read ans
+   if [ $ans -eq $yes ]
+   then
+      echo "Found..."
+   else
+      echo "Greater or smaller(1 G/0 S): "
+      read ans1
+      if [ $ans1 -eq $yes ]
+      then
+         start=$temp2
+      else
+         end=$temp2
+      fi
+   fi
+done

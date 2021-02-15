7faea81f13259362118a05b47ba115ac88b3ad71
samshine <sameershinde23243@gmail.com>
2021-02-15 14:39:59 +0530

    example1 code added

--- while.sh
+++ while.sh
@@ -1,1 +1,14 @@
-echo "while loop problems"
+read -p "Enter N:" N
+i=1
+while [ $i -le $N ]
+do
+   j=1
+   mul=1
+   while [ $j -le $i ]
+   do
+      mul=$(($mul*2))
+      j=$(($j+1))
+   done
+echo "value=" $mul
+i=$(($i+1))
+done

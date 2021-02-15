d8d597b7c27517c1db6bc56f50bc119b055321ee
samshine <sameershinde23243@gmail.com>
2021-02-15 14:41:52 +0530

    example3 code added

--- while.sh
+++ while.sh
@@ -1,1 +1,18 @@
 echo "while loop problems"
+
+
+head=1
+tail=0
+countHead=0
+counTails=0
+while [ $countHead -lt "11" ]
+do
+   randomNumber=$(($RANDOM%2))
+   if [ $randomNumber -eq "1" ]
+   then
+      countHead=$(($countHead+1))
+      echo "Head wins(count):" $countHead
+   else
+      echo "Tail wins"
+   fi
+done
